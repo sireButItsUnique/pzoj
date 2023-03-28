bd47805214ade2952a1cd264dfe2c034c3e24a48
sire <robertcxlee@gmail.com>
2023-03-28 17:45:28 -0600

    got rid of useless stuff

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,3 +1,7 @@
 export default () => {
-	return <></>;
+	return (
+		<div className="place-self-center content-center justify-center flex-col bg-dark-0">
+			<h1 className="text-blue-0">PZOJ</h1>
+		</div>
+	);
 };
