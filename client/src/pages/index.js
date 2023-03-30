f5b2e01be35a3e77c60bba46c2659060fd797e24
sire <robertcxlee@gmail.com>
2023-03-30 13:11:56 -0600

    set up basic gui + background

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,7 +1,17 @@
+import Head from "next/head";
+
 export default () => {
 	return (
-		<div className="place-self-center content-center justify-center flex-col bg-dark-0">
-			<h1 className="text-blue-0">PZOJ</h1>
-		</div>
+		<>
+			<Head>
+				<title>PZOJ</title>
+			</Head>
+
+			<main className="bg-dark-0">
+				<div className="place-self-center content-center justify-center flex-col bg-dark-0">
+					<h1 className="text-blue-0">PZOJ</h1>
+				</div>
+			</main>
+		</>
 	);
 };
